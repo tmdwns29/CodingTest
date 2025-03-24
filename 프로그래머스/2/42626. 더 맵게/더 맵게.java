3e970e400bbb372d2a937ad9b0bd58b5c829d786
YooSeungJun <12000ha@naver.com>
2025-03-24 20:40:40 +0900

    [level 2] Title: 더 맵게, Time: 1427.68 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int cnt = 0;
-        for (int i=0; i<scoville.length; i++) {
-            pq.add(scoville[i]);
-        }
+        
+        for (int sc: scoville) pq.add(sc);
         
         while (pq.size() >= 2) {
             int lv1 = pq.poll();
@@ -17,8 +15,8 @@ class Solution {
             }
             int lv2 = pq.poll();
             int shakeLv = lv1 + (lv2*2);
-            cnt++;
             pq.add(shakeLv);
+            cnt++;
         }
 
         return pq.peek() >= K ? cnt : -1;
