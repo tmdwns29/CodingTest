a2ecc44b73a12550d5419b390a7837e733b5c8a1
YooSeungJun <12000ha@naver.com>
2025-04-05 00:30:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.16 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,19 +2,23 @@ import java.util.*;
 
 class Solution {
     static int[] visited;
-    static boolean[][] adjList;
+    static ArrayList<Integer>[] adjList;
     
     public int solution(int n, int[][] edge) {
 
         int m = edge.length;
-        adjList = new boolean[n+1][n+1];
+        adjList = new ArrayList[n+1];
         visited = new int[n+1];
+        
+        for (int i=1; i<n+1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
         
         for (int i=0; i<m; i++) {
             int a = edge[i][0];
             int b = edge[i][1];
-            adjList[a][b] = true;
-            adjList[b][a] = true;
+            adjList[a].add(b);
+            adjList[b].add(a);
         }
         return bfs(1);
     }
@@ -28,11 +32,12 @@ class Solution {
         
         while (!queue.isEmpty()) {
             int c = queue.poll();
-            for (int i=1; i<adjList.length; i++) {
-                if (adjList[c][i] && visited[i] == 0) {
-                    queue.add(i);
-                    visited[i] = visited[c] + 1;
-                    maxNum = Math.max(visited[i], maxNum);
+            for (int i=0; i<adjList[c].size(); i++) {
+                int num = adjList[c].get(i);
+                if (visited[num] == 0) {
+                    queue.add(num);
+                    visited[num] = visited[c] + 1;
+                    maxNum = Math.max(visited[num], maxNum);
                     
                 }
             }
