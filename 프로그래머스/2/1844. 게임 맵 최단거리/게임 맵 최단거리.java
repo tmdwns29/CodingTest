b25f687562e6935d85159476fdea8574247cf000
YooSeungJun <12000ha@naver.com>
2025-04-04 23:35:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.60 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -14,13 +14,12 @@ class Solution {
         int answer;
         int N = maps.length;
         int M = maps[0].length;
-        boolean[][] visited = new boolean[N][M];
+
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
         
         Queue<Point> queue = new ArrayDeque<>();
         queue.add(new Point(0, 0));
-        visited[0][0] = true;
         
         while (!queue.isEmpty()) {
             Point p = queue.poll();
@@ -29,19 +28,13 @@ class Solution {
             for (int d=0; d<4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
-                if (nr>=0 && nr<N && nc>=0 && nc<M && maps[nr][nc] == 1 && !visited[nr][nc]) {
+                
+                if (nr>=0 && nr<N && nc>=0 && nc<M && maps[nr][nc] == 1) {
                     queue.add(new Point(nr, nc));
                     maps[nr][nc] = maps[r][c] + 1;
-                    visited[nr][nc] = true;
                 }
             }
         }
-        // for (int[] s : maps) {
-        //     for (int n : s) {
-        //         System.out.print(n+" ");
-        //     }
-        //     System.out.println();
-        // }
         
         answer = maps[N-1][M-1] == 1 ? -1 : maps[N-1][M-1];
         return answer;
