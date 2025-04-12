a2d3c666f10c4b9a5565a22336eb2abfc5e5f628
YooSeungJun <12000ha@naver.com>
2025-04-12 20:12:20 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 168 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextInt()*3 + input.nextInt()*4 + input.nextInt()*5);
+    }
+}
