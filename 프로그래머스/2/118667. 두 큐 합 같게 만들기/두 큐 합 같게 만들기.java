785a18aeba13b857b42aea4993ac82f86a34b7d9
YooSeungJun <12000ha@naver.com>
2025-05-07 14:33:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 88.18 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
         Queue<Long> que1 = new ArrayDeque<>();
         Queue<Long> que2 = new ArrayDeque<>();
         long sum1=0, sum2=0;
-        int cnt=0;
-        
+        int answer=0;
         for (long q1 : queue1) {
             sum1 += q1;
             que1.add(q1);
@@ -16,7 +14,7 @@ class Solution {
             sum2 += q2;
             que2.add(q2);
         }
-        while (sum1 != sum2 && cnt <= 600_000) {
+        while (sum1 != sum2 && answer <= 300_000) {
             if (sum1 > sum2) {
                 long c = que1.poll();
                 que2.add(c);
@@ -28,24 +26,8 @@ class Solution {
                 sum1 += c;
                 sum2 -= c;
             }
-            cnt++;
+            answer++;
         }
-        
-        
-        
-        /*
-        3, 2, 7, 2 = 14
-        4, 6, 5, 1 = 16
-        
-        3, 2, 7, 2, 4 = 18
-        6, 5, 1 = 12
-        
-        2, 7, 2, 4 = 15
-        6, 5, 1, 3 = 15
-        */
-        
-        answer = sum1 != sum2 ? -1 : cnt;
-        
-        return answer;
+        return sum1 != sum2 ? -1 : answer;
     }
 }
